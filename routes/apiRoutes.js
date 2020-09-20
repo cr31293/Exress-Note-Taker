102d05fad528e127e96f5820b05681f7560f3a9f
Chris Reed <cr31293@gmail.com>
2020-09-20 18:32:34 -0400

    Notes app working can save notes now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,5 @@
+const { json } = require("body-parser");
+const { RSA_NO_PADDING } = require("constants");
 // * The following API routes should be created:
 
 //   * GET `/api/notes` - Should read the `db.json` file and return all 
@@ -7,29 +9,72 @@
 //   body, add it to the `db.json` file, and then return the new note to the 
 //   client.
 var express = require("express");
-var notes = require("../Develop/db/db.json");
+var router = express.Router();
+var fs = require("fs");
+var notes = require("../db/db.json");
+var path = require("path");
+
+
 
 module.exports = function(app) {
 
-app.get("/api/notes", function(req, res) {
-   res.json(notes);
-});
+    app.get("/api/notes", function(req, res) {
+        res.json(notes);
+    });
 
-app.post("/api/notes", function(req, res) {
-    console.log(req.body);
-    const note = req.body;
+    app.post("/api/notes", function(req, res) {
+        var note = {
+            id: notes.length,
+            title: req.body.title,
+            text: req.body.text
+        };
+        notes.push(note);
 
-    notes.push(note);
-    res.json(note);
-});
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err, res) {
+            if (err) {
+                throw err;
+            }
+        })
+        res.json(note);
+    });
 
-// if we want to clear out the db
-app.post("/api/clear", function(req, res) {
-    // empty out array
-    notes.length = 0;
+    // app.delete("/api/notes/:id", function(req, res) {
+    //     if (notes.id = req.params.id) {
+    //         notes.splice(notes.id,0,);
+    //     }
+    // })
 
-    res.json({ ok: true });
-})
+
+
+// app.get("/api/notes", function(req, res) {
+//     notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+//     res.json(notes);
+// });
+
+// app.post("/api/notes", function(req, res) {
+//     var note = {
+//         id: Math.floor(Math.random()),
+//         text: req.body.title,
+//     };
+//     notes.push(note);
+
+//     fs.writeFileSync("./db/db.json", JSON.stringify(notes, function(err, res) {
+//         if (err) {
+//             throw err;
+//         }
+//     }))
+//     res.json(note);
+// });
+
+
+
+// // if we want to clear out the db
+// app.post("/api/clear", function(req, res) {
+//     // empty out array
+//     notes.length = 0;
+
+//     res.json({ ok: true });
+// })
 
 };
 
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 
+
+
 // set up express app
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 
 // set up express app to handle parsing
 app.use(express.urlencoded({ extended: true }));
@@ -13,9 +14,6 @@ app.use(express.json());
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-
-
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
